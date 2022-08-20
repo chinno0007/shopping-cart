3ea7e09712c7e66ab1892695ae973909fa67b490
FrameworkTV <email1@example.com>
2022-08-20 21:31:01 +0600

    first commit

---
+++ js/common.js
@@ -0,0 +1,29 @@
+
+function getTetxElementValueById(elementId) {
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+
+}
+function setTextElementValueById(elementId, value) {
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
+function calculateSubTotal() {
+    const currentPhoneTotal = getTetxElementValueById('phone-total');
+
+    const currentCaseTotal = getTetxElementValueById('case-total');
+    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+
+    setTextElementValueById('sub-total', currentSubTotal)
+
+    // calculate tax
+
+    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementValueById('tax-amount', taxAmount);
+
+    const finalAmount = currentSubTotal + taxAmount;
+    setTextElementValueById('final-total', finalAmount);
+}
---
+++ js/phone.js
@@ -0,0 +1,49 @@
+function updatePhoneNumber(isIncrease) {
+
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    let newPhoneNumber;
+    if (isIncrease) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalELement = document.getElementById('phone-total');
+    phoneTotalELement.innerText = phoneTotalPrice;
+
+}
+
+
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+
+    const newPhoneNumber = updatePhoneNumber(true);
+
+    updatePhoneTotalPrice(newPhoneNumber);
+
+    calculateSubTotal();
+    // calculate total
+
+
+
+
+});
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+
+
+    const newPhoneNumber = updatePhoneNumber(false);
+
+    updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
+})
